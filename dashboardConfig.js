c9afa5be2853d748a3a225d952a836111bc79b0a
Corey Hayden <cghayden@gmail.com>
2021-06-16 13:46:48 -0400

    basic studio dashboard installed

---
+++ dashboardConfig.js
@@ -0,0 +1,17 @@
+export default {
+  widgets: [
+    {
+      name: 'project-info',
+    },
+    {
+      name: 'project-users',
+    },
+    // {
+    //   name: 'document-list',
+    //   options: {
+    //     title: 'Last edited documents',
+    //     order: '_updatedAt desc',
+    //   },
+    // },
+  ],
+};
