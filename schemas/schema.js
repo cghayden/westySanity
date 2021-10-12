4e90a02a17b42226ac02d1058c146d1e800d8966
Corey Hayden <cghayden@gmail.com>
2021-10-12 09:12:26 -0400

    removed textblock type from schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -14,13 +14,13 @@ import eventsPage from './documents/eventsPage';
 import aboutPage from './documents/aboutPage';
 import contactPage from './documents/contactPage';
 import order from './documents/order';
+import siteSettings from './documents/siteSettings';
 
 // object types
 import bodyPortableText from './objects/bodyPortableText';
 import excerptPortableText from './objects/excerptPortableText';
 import mainImage from './objects/mainImage';
 import backgroundImage from './objects/backgroundImage';
-import siteSettings from './documents/siteSettings';
 import coffeeOrderItem from './objects/coffeeOrderItem';
 
 // Then we give our schema to the builder and provide the result to Sanity
